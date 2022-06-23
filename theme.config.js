4204ca198ccad596e8cdda11211c1ea9583ee85c
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-06-23 09:13:50 -0400

    Update theme.config.js
    Footer edit link changed from 'Edit this page on GitHub' to footerEditLink: False

--- theme.config.js
+++ theme.config.js
@@ -61,7 +61,7 @@ export default {
   prevLinks: true,
   nextLinks: true,
   footer: true,
-  footerEditLink: 'Edit this page on GitHub',
+  footerEditLink: false,
   footerText: <>Powered By {new Date().getFullYear()} Sokoni ©</>,
   unstable_faviconGlyph: '👋',
 }
